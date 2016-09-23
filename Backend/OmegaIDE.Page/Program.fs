f619b19dbab36c5e8bb0d19b36146e5c5d6c7117
Harlan Connor <harlan.connor@gmail.com>
2016-09-23 20:42:34 +0100

    Almost finished the sytax highlighting parser

--- Backend/OmegaIDE.Page/Program.fs
+++ Backend/OmegaIDE.Page/Program.fs
@@ -1,7 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+open C_Omega
+open C_Omega.ArraySliceImprovement
+open C_Omega.Helpers
+open OmegaIDE.FSharp
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let v = "@comment@//.*\n" |> Modules.Highlighting.SyntaxHighlighter.OfString
+    v.update "//hi\n//hi again\n" |> printfn "%A"
     0 // return an integer exit code
 
