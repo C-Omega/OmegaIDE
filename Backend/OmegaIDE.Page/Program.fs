864ee582be31c9dc576b15c6ebfdcd8f6f86bcd0
Cyclic3 <harlan.connor@gmail.com>
2016-09-26 09:59:04 +0100

    Working on IPC & config, and making parser better

--- Backend/OmegaIDE.Page/Program.fs
+++ Backend/OmegaIDE.Page/Program.fs
@@ -4,9 +4,32 @@ open C_Omega
 open C_Omega.ArraySliceImprovement
 open C_Omega.Helpers
 open OmegaIDE.FSharp
+open OmegaIDE.FSharp.IPC
+open OmegaIDE.FSharp.ProjectFile
+open OmegaIDE.FSharp.Modules.Highlighting
+open OmegaIDE.FSharp.Config
+open OmegaIDE.FSharp.Tree
+open System.IO
+let a = ep loopbackv4 9011
+let b = ep loopbackv4 9012
 [<EntryPoint>]
 let main argv = 
-    let v = "@comment@\(\*.*\*\);\n@comment@//.*\\n" |> Modules.Highlighting.SyntaxHighlighter.OfString
-    v.update "normal(*comment*)\n//comment 2\n" |> printfn "%A"
+    {conf = [Branch("branch",[Leaf("leaf","value")])]} |> side (printfn "%A") |> string |> side (printfn "%A") |> Config.OfString |> printfn "%A"
+    (*
+    let ipc = IPC(ep (ip argv.[0]) 0, ep (ip argv.[0]) (int argv.[1]))
+    let config = File.ReadAllText argv.[2]
+    while true do 
+        match ipc.Receive() with
+        |{parts = String "load"::String what::String path::[]} ->
+            match what with
+            |"project" -> 
+
+            |"highlighter" -> SyntaxHighlighter.OfString
+
+    ipc1.Send  {parts = [Bytes[|0uy|];String "hi";KeyValue("foo","bar")]}
+    //let v = "@comment@\(\*.*\*\);\n@comment@//.*\\n" |> Modules.Highlighting.SyntaxHighlighter.OfString
+    //v.update "normal(*comment*)\n//comment 2\n" |> printfn %A
+    ipc2.Receive() |> printfn %A
+    *)
     0 // return an integer exit code
 
