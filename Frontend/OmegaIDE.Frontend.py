a2e814b4d97c59cdbbec9eddf244ac81e31e9a68
saulsmcouk <ssm8@hotmail.co.uk>
2016-10-04 14:27:27 +0100

    Merge conflicts FIXED

--- Frontend/OmegaIDE.Frontend.py
+++ Frontend/OmegaIDE.Frontend.py
@@ -28,18 +28,16 @@ class App(QtGui.QMainWindow):
         self.init_toolbar()
         
         self.init_menubar()
-
-<<<<<<< HEAD
         self.init_status_bar()
         
-=======
+
         self.status_bar = self.statusBar()
         self.status_bar_text = QtGui.QLabel()
         self.status_bar_text.setText("")
         self.status_bar.addWidget(self.status_bar_text)
 
-    def init_formatbar(self):
->>>>>>> master
+    
+
 
     
     def init_status_bar(self):
@@ -136,27 +134,12 @@ class App(QtGui.QMainWindow):
         self.file = ""
 
     def show_cursor_pos(self):
-<<<<<<< HEAD
 
         c = self.text.getCursorPosition()
-=======
-        c = self.text.getCursorPosition()
-        
->>>>>>> master
-        # c = self.text.textCursor()
 
         line = c[0] + 1 #MERE MORTALS LIKE THEIR LINES TO BEGIN AT 1
         col = c[1] + 1 #MERE MORTALS LIKE THEIR COLS TO BEGIN AT 1
-<<<<<<< HEAD
-
         self.status_bar_text.setText("{}::{}".format(line,col))
-        #self.status_bar_text.setText
-        #self.status_bar.clearMessage()
-        #self.status_bar.showMessage("{}::{}".format(line,col))
-=======
-        
-        self.status_bar_text.setText("{}::{}".format(line,col))
->>>>>>> master
 
 def main():
 
